decf2cbcf6ad44a78d335b5c1e39a69e945bc422
seleshabani <46116554+seleshabani@users.noreply.github.com>
2020-09-12 11:47:08 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,14 @@
 <?php
+use App\App;
+use Delight\Auth\Auth;
+use function \DI\{create,factory};
+use Psr\Container\ContainerInterface;
 
-return [];
+$db=App::getdb();
+
+return [
+    "site.name" => "http://blog.test",
+    Auth::class => factory(function (ContainerInterface $c) use ($db){
+        return new Auth($db);
+    })
+];
