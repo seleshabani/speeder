bf1b67febb74cd3690c4d23cc2a3335df15dcb5d
seleshabani <46116554+seleshabani@users.noreply.github.com>
2020-09-12 11:42:01 +0100

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -6,25 +6,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 require '../vendor/autoload.php';
-//$routes=require '../config/Routes.php';
-$dependences=require '../config/dependecies.php';
+$coreDependances = require '../config/dependecies.php';
+$userDependances = require '../config.php';
+$coreDependances = array_merge($coreDependances,$userDependances);
 
 //configuration du container externe utilisé
 $builder = new ContainerBuilder();
-$builder->addDefinitions($dependences);
-$Dicontainer=$builder->build();
+$builder->addDefinitions($coreDependances);
+$Dicontainer = $builder->build();
 
 //création du container interne en lui injectant le conteneur externe installé
 $container = new Speeder\InjectionContainer\Container($Dicontainer);
 
 //creation de la request et de la reponse en remplissant la req des informations global
-$routes=$container->get('_routes');
-$request=HttpFoundationRequest::createFromGlobals();
-$response=new Response();
+$routes = $container->get('_routes');
+$request = HttpFoundationRequest::createFromGlobals();
+$response = new Response();
 
 Debugger::Init($request,$response,$routes,$container);
 
-$app=new App($container);//passage du container à l'application
+$app = new App($container);//passage du container à l'application
  
- $response=$app->HandleBySymfonyComponent($request,$response,$routes);
- $response->send();
+$response = $app->HandleBySymfonyComponent($request,$response,$routes);
+$response->send();
