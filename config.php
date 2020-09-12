22971f2e20b7af480e5a88ea17cf578fa70148e5
seleshabani <46116554+seleshabani@users.noreply.github.com>
2020-09-12 11:52:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ use Psr\Container\ContainerInterface;
 $db=App::getdb();
 
 return [
-    "site.name" => "http://blog.test",
+    "site.name" => "http://adresse_site",
     Auth::class => factory(function (ContainerInterface $c) use ($db){
         return new Auth($db);
     })
